39dc4695043073d33db46d583a6f110a893846cc
Jriera <jordiriera81@gmail.com>
2021-10-14 17:21:38 +0200

    chore: adding identifiers to calculator inputs

--- fetcher.js
+++ fetcher.js
@@ -2,14 +2,7 @@
 
 
 
-const getExchangeRateLast = async (base_currency, output_currency) => {
-    const response = await axios.get(`https://exchange-rates-reuters.herokuapp.com/pairs?from=${base_currency}&to=${output_currency}`);
-    const data = response.data
-    const last = response.data.currencypairs[0].last
 
-    console.log(data);
-    return last;
-}
 
 const getExchangeRateChange = async (base_currency, output_currency) => {
     const response = await axios.get(`https://reuters-exchange-rates.p.rapidapi.com/pairs?from=${base_currency}&to=${output_currency}`, {
