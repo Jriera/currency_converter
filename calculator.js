c880bd6b5588d5ea6cbb79c8a34ae19c43fcc7da
Jriera <jordiriera81@gmail.com>
2021-10-14 18:07:58 +0200

    feat:Basic calculator functionality, one direction

--- calculator.js
+++ calculator.js
@@ -1,3 +1,5 @@
+/* var axios = require('axios') */
+
 const getExchangeRateLast = async (base_currency, output_currency) => {
     const response = await axios.get(`https://exchange-rates-reuters.herokuapp.com/pairs?from=${base_currency}&to=${output_currency}`);
     const data = response.data
@@ -6,4 +8,22 @@ const getExchangeRateLast = async (base_currency, output_currency) => {
     console.log(data);
     return last;
 }
+
+const input = document.getElementById('input-amount');
+const output = document.getElementById('output-amount');
+const from = document.getElementById('from');
+const to = document.getElementById('to');
+
+from.addEventListener('change', async () => {
+    const inputAmount = input.value;
+    const outputAmount = output.value;
+    const from_currency = from.value;
+    const to_currency = to.value;
+
+    const rate = await getExchangeRateLast(from_currency, to_currency);
+    output.value = inputAmount * rate;
+    console.log(output.value);
+
+
+})
 
